ec1d752b582b85f9019ffdafe3891eff4def7339
hassanAlAwwadi <hassan.awwadi@gmail.com>
2019-11-10 15:45:13 +0100

    backspage in the highscore view will now return to the mainmenu

--- src/Game.hs
+++ src/Game.hs
@@ -9,7 +9,8 @@ import qualified Data.Ord as D (Down(..), comparing)
 
 import Classess as C
 import Resources as R(StaticResource, DynamicResource)
-import World as W(World, lives, score, stat, dyn)
+import qualified World as W(stat, dyn, score)
+import World(World, lives)
 import Menu(Menu, createMenu, menuAction)
 
 import Graphics.Gloss.Interface.IO.Game
@@ -17,8 +18,8 @@ import Graphics.Gloss.Interface.IO.Game
 data Game   = Playing      { world :: World} 
             | Menu         { menu  :: Menu (IO Game) } 
             -- | Highcores are really the only reason we need to use playIO instead of regular play
-            | HighScores   { mvps  :: [(String, Float)], scrollUp :: Bool, scrollDown :: Bool, scrollDelta :: Float}
-            | NewHighScore { nameWIP :: String, score :: Int}
+            | HighScores   { mvps  :: [(String, Float)], scrollUp :: Bool, scrollDown :: Bool, scrollDelta :: Float, stat :: StaticResource, dyn :: DynamicResource }
+            | NewHighScore { nameWIP :: String, score :: Int, stat :: StaticResource, dyn :: DynamicResource }
 
 
 instance Creatable Game where
@@ -27,7 +28,7 @@ instance Creatable Game where
 mainMenu :: StaticResource -> DynamicResource -> Menu (IO Game)
 mainMenu s d = createMenu [
     ("play game", pure $ Playing w), 
-    ("HighScore", loadHighScore (readOrCreateFile  "HighScores.txt") ),
+    ("HighScore", loadHighScore s d "HighScores.txt" ),
     ("Quit Game", exitSuccess)
     ] where
     w = create s d
@@ -35,17 +36,17 @@ mainMenu s d = createMenu [
 pauseMenu :: World -> Menu (IO Game)
 pauseMenu  w = createMenu [
     ("Resume game", pure $ Playing w), 
-    ("Main menu", pure $ Menu $ mainMenu (stat w) (dyn w) ),
+    ("Main menu", pure $ Menu $ mainMenu (W.stat w) (W.dyn w) ),
     ("Quit Game", exitSuccess)
     ]
 
 --loading the highscores
-loadHighScore :: IO String -> IO Game
-loadHighScore file = do 
-    file' <- file
+loadHighScore :: StaticResource -> DynamicResource -> String -> IO Game
+loadHighScore st dn fileName = do 
+    file' <- readOrCreateFile fileName
     let filelines = lines file'
     let scores = sortBy (D.comparing $ D.Down . snd) $ map read filelines
-    return $ HighScores { mvps = scores, scrollDown = False, scrollUp = False, scrollDelta = 0 }
+    return $ HighScores { mvps = scores, scrollDown = False, scrollUp = False, scrollDelta = 0, stat = st, dyn = dn }
 
 --write the highscores
 writeHighScore :: Int -> IO String
@@ -72,7 +73,7 @@ instance Paint Game where
         in translate (-900) (sd + 400) single
     --paint game
     paint Playing{ world = w } = paint w
-    paint NewHighScore{ nameWIP = n, Game.score = s} = pictures $ map (color white) [
+    paint NewHighScore{ nameWIP = n, score = s} = pictures $ map (color white) [
         translate (-700) 0      $ text $ "Your Name: \n" ++ n ++ "_", 
         translate (-700) (-250) $ text $ "Your Score: " ++ show s
         ]
@@ -91,26 +92,27 @@ instance HandleIO Game where
     handleIO e g@Playing{ world = w } = return $ g{ world = handle e w }
     
     --WriteHighScore 
-    handleIO e g@NewHighScore{ nameWIP = n, Game.score = s} = case e of 
+    handleIO e g@NewHighScore{ nameWIP = n, score = s, stat = st, dyn = dn} = case e of 
         EventKey (Char '\b') Down _ _ -> return $ g{ nameWIP = take (length n -1) n}
         EventKey (Char c) Down _ _ -> return $ if isAlphaNum c then g{ nameWIP = n ++ [c]} else g
         EventKey (SpecialKey KeySpace) Down _ _ -> return $ g{nameWIP = n ++ " "}
         -- WIP: EventKey (SpecialKey KeyBackspace) Down _ _ -> return $ g{nameWIP = n ++ " "}
         EventKey (SpecialKey KeyEnter) Down _ _ -> do
             appendFile "HighScores.txt" $ '\n' : show (n, s)
-            loadHighScore (readOrCreateFile  "HighScores.txt")
+            loadHighScore st dn "HighScores.txt"
         _ -> return g
     -- you can scroll up and down for highscores
-    handleIO e g@HighScores{} =pure $ case e of
-        EventKey (Char 'w') Down _ _ -> g{scrollUp   = True}
-        EventKey (Char 's') Down _ _ -> g{scrollDown = True}
-        EventKey (Char 'w') Up   _ _ -> g{scrollUp   = False}
-        EventKey (Char 's') Up   _ _ -> g{scrollDown = False}
-        _                            -> g
+    handleIO e g@HighScores{stat = st, dyn = dn} =pure $ case e of
+        EventKey (Char 'w' ) Down _ _ -> g{scrollUp   = True}
+        EventKey (Char 's' ) Down _ _ -> g{scrollDown = True}
+        EventKey (Char 'w' ) Up   _ _ -> g{scrollUp   = False}
+        EventKey (Char 's' ) Up   _ _ -> g{scrollDown = False}
+        EventKey (Char '\b') Down _ _ -> Menu $ mainMenu st dn
+        _                             -> g
 
 instance Tick Game where
-    tick f g@Playing{ world = w } | lives w == 0 = NewHighScore {nameWIP = "", Game.score = W.score w}
-                                    | otherwise  =  g{ world = tick f w }
+    tick f g@Playing{ world = w } | lives w == 0 = NewHighScore {nameWIP = "", Game.score = W.score w, stat = W.stat w, dyn = W.dyn w}
+                                  | otherwise  =  g{ world = tick f w }
     -- menu/highscore tick doesn't do anything
     tick _ g@HighScores{scrollUp = up, scrollDown = down, scrollDelta = d} 
         | up   = g{scrollDelta = d + 5}
